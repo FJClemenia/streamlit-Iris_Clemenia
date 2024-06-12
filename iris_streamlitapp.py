212d38794de88e2615e669fc19cb793e14f68657
FJClem <joshua.clemenia@gmail.com>
2024-06-13 00:27:15 +0800

    changes to irisapp main

--- iris_streamlitapp.py
+++ iris_streamlitapp.py
@@ -29,24 +29,24 @@ st.write(stats)
 
 # Create a scatter plot for the averages of each feature for the selected species
 fig, ax = plt.subplots(figsize=(8, 8))
-sns.scatterplot(x='sepal length (cm)', y='sepal width (cm)', data=species_df)
-ax.set_title("Averages for", species)
+sns.scatterplot(x='sepal_length', y='sepal_width', data=species_df)
+ax.set_title(f"Averages for {species}")
 st.pyplot(fig)
 
 # Create a scatter plot for the averages of each feature for the selected species
 fig, ax = plt.subplots(figsize=(8, 8))
-sns.scatterplot(x='petal length (cm)', y='petal width (cm)', data=species_df)
-ax.set_title("Averages for", species)
+sns.scatterplot(x='petal_length', y='petal_width', data=species_df)
+ax.set_title(f"Averages for {species}")
 st.pyplot(fig)
 
 # Create a scatter plot for the averages of each feature for the selected species
 fig, ax = plt.subplots(figsize=(8, 8))
-sns.scatterplot(x='sepal length (cm)', y='petal length (cm)', data=species_df)
-ax.set_title("Averages for", species)
+sns.scatterplot(x='sepal_length', y='petal_length', data=species_df)
+ax.set_title(f"Averages for {species}")
 st.pyplot(fig)
 
 # Create a scatter plot for the averages of each feature for the selected species
 fig, ax = plt.subplots(figsize=(8, 8))
-sns.scatterplot(x='sepal width (cm)', y='petal width (cm)', data=species_df)
-ax.set_title("Averages for", species)
+sns.scatterplot(x='sepal_width', y='petal_width', data=species_df)
+ax.set_title(f"Averages for {species}")
 st.pyplot(fig)
